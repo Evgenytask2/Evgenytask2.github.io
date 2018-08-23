9f97d2255512b80e03c0a2fb0005aebcea506322
Evgeny T <official.trusov.mail@gmail.com>
2018-08-23 13:33:33 +0700

    Intrudoced mixins in stages block

--- modules/main.js
+++ modules/main.js
@@ -9,6 +9,7 @@ import standardBtns from "./standardBtns/script.js";
 import arrowBtns from "./arrowBtns/script.js";
 import circleDiagrams from "./percentages/script.js";
 import sliders from "./sliders/script.js";
+import stages from "./stages/script.js";
 
 import media from "./projectShare/media.less"; // must be included as the last file
 
