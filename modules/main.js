b9a1048b2ca4d3740a010741f977053c8540916c
Evgeny T <official.trusov.mail@gmail.com>
2018-08-24 00:39:58 +0700

    No longer script required for stages. Introduced mixins for toggles

--- modules/main.js
+++ modules/main.js
@@ -4,6 +4,9 @@ try {
     req.keys().forEach((key) => req(key)); 
 } catch(ex) {} 
 
+let pugTemplate = require("./root/index.pug");
+document.querySelector("#pugTemplate").innerHTML = pugTemplate;
+
 import root from "./root/script.js";
 import standardBtns from "./standardBtns/script.js";
 import arrowBtns from "./arrowBtns/script.js";
@@ -11,17 +14,7 @@ import circleDiagrams from "./percentages/script.js";
 import sliders from "./sliders/script.js";
 import stages from "./stages/script.js";
 import formElements from "./formElements/script.js";
+import switches from "./switches/script.js";
 
 import media from "./projectShare/media.less"; // must be included as the last file
 
-
-let pugTemplate = require("./root/index.pug");
-document.querySelector("#pugTemplate").innerHTML = pugTemplate;
-
-let elems = document.querySelectorAll(".stageChart__clickBox");
-for(let elem of elems) 
-    elem.onclick = function() {
-        for(let e of elems)
-             if(this != e)
-                e.checked = false;
-        }
