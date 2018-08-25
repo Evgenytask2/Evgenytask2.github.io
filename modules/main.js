544514410292d0dcf750336f9115b93b63a9de26
Evgeny T <official.trusov.mail@gmail.com>
2018-08-25 13:47:42 +0700

    Enhanced location block

--- modules/main.js
+++ modules/main.js
@@ -19,6 +19,7 @@ import search from "./searchTools/script.js";
 import userProfile from "./userProfile/script.js";
 import news from "./news/script.js";
 import events from "./events/script.js";
+import location from "./location/script.js";
 
 import media from "./projectShare/media.less"; // must be included as the last file
 
